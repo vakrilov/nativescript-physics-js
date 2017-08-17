223321b8c014b4b5f5213f6a972e2c078bed8809
vakrilov <alexander.vakrilov@telerik.com>
2017-08-17 09:42:31 +0300

    Version 2 Compatible with NS 3

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
+"use strict";
 var Physics = require("./physics/physicsjs-full");
 var nsRenderer = require("./physics/ns-renderer");
 module.exports = Physics;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSxJQUFJLE9BQU8sR0FBRyxPQUFPLENBQUMsMEJBQTBCLENBQUMsQ0FBQztBQUNsRCxJQUFJLFVBQVUsR0FBRyxPQUFPLENBQUMsdUJBQXVCLENBQUMsQ0FBQztBQUNsRCxpQkFBUyxPQUFPLENBQUMifQ==
